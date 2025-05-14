932fb0a3d00b867a1b3346cd9ae6d9eb4776011b
deryabeyoglukarahan <derya.beyoglu1@gmail.com>
2025-05-14 22:52:30 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Bir limit değeri giriniz:");
+int limit = Convert.ToInt32(Console.ReadLine());
+
+//While Döngüsü
+int sayac= 0;
+while (sayac <= limit)
+{
+    Console.WriteLine("Ben bir Patika'lıyım");
+    sayac++;
+}
+
+//Do-While Döngüsü
+int sayac2 = 0;
+do{
+    Console.WriteLine("Ben bir Patika'lıyım");
+    sayac2++;
+}while (sayac2 <= limit);
+
+
+/*
+WHILE döngüsü önce koşulu kontrol eder, sonra çalışır. Bu yüzden limit negatif olduğunda hiç çalışmaz.
+Örnek: limit = -5 → while döngüsü çalışmaz.
+        
+DO-WHILE döngüsü önce çalışır, sonra koşulu kontrol eder. Bu yüzden limit negatif bile olsa en az bir kez çalışır.
+Örnek: limit = -5 → do-while döngüsü ekrana bir kez "Ben bir Patika'lıyım" yazar.
+*/
+
+
